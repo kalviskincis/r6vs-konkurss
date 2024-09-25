4073dfeeb537573c1e200dcbae44c9f2400022f2
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2024-09-25 20:35:27 +0300

    Uzlabota konkura kritēriju rādīšana. Iespēja norādīt dalībnieku kā novērtētu.

--- konkurss_app.py
+++ konkurss_app.py
@@ -21,7 +21,7 @@ def vertet(zurija):
     FROM personas 
     INNER JOIN iestades ON personas.iestade = iestades.id 
     INNER JOIN pieteikumi ON personas.id = pieteikumi.dalibnieks    
-    WHERE personas.loma = 1;
+    WHERE personas.loma = 1 AND pieteikumi.novertets = 0;
     """
 
     dalibnieki_atbilde = conn.execute(dalibnieki_vaicajums)
@@ -41,18 +41,22 @@ def vertet(zurija):
             "punkti5": 0
         }
 
-        for kriterijs in punkti:
+        kriteriji = ["Intonācija", "Ritms", "Ansamblis", "Interpretācija", "Kopiespaids"]
+
+        for kriterija_punkti, kriterija_nosaukums in zip(punkti, kriteriji):
             ievadits = False
             while not ievadits:
                 try:
-                    vertiba = int(input(f"Ievadi punktus (0 līdz 10) par {kriterijs}"))
+                    vertiba = int(input(f"Ievadi punktus (0 līdz 10) par {kriterija_nosaukums}: "))
                     if vertiba in range(0, 11):
-                        punkti[kriterijs] = vertiba
+                        punkti[kriterija_punkti] = vertiba
                         ievadits = True
                     else:
-                        print(f"Vērtējumam par {kriterijs} jābūt veselam skaitlim robežās starp 0 un 10.")
+                        print(f"Vērtējumam par {kriterija_nosaukums} jābūt veselam skaitlim robežās starp 0 un 10.")
                 except ValueError:
-                    print(f"Vērtējumam par {kriterijs} jābūt veselam skaitlim robežās starp 0 un 10.")
+                    print(f"Vērtējumam par {kriterija_nosaukums} jābūt veselam skaitlim robežās starp 0 un 10.")
+
+        komentars = input("Ievadiet komentāru par priekšnesumu. (Nav obligāti. Lai atstātu tukšu, spiediet Enter.")
 
         vertejums = {}
         vertejums["zurijas_parstavis"] = zurija["id"]
@@ -60,8 +64,9 @@ def vertet(zurija):
         vertejums["prieksnesums"] = katrs["prieksnesums"]
         for kriterijs in punkti:
             vertejums[kriterijs] = punkti[kriterijs]
+        vertejums["komentars"] = komentars
 
-        conn.execute("INSERT INTO konkurss (zurijas_parstavis, dalibnieks, prieksnesums, punkti1, punkti2, punkti3, punkti4, punkti5) VALUES (:zurijas_parstavis, :dalibnieks, :prieksnesums, :punkti1, :punkti2, :punkti3, :punkti4, :punkti5)", vertejums)
+        conn.execute("INSERT INTO konkurss (zurijas_parstavis, dalibnieks, prieksnesums, punkti1, punkti2, punkti3, punkti4, punkti5, komentars) VALUES (:zurijas_parstavis, :dalibnieks, :prieksnesums, :punkti1, :punkti2, :punkti3, :punkti4, :punkti5, :komentars)", vertejums)
         conn.commit()
 
 
@@ -81,8 +86,19 @@ def rezultati():
     atbilde = conn.execute(vaicajums).fetchall()
     rezultati = []
     for katrs in atbilde:
-        rezultati.append(dict(katrs))
+        dalibnieka_vertejums = dict(katrs)
+        if dalibnieka_vertejums["Punkti"] >= 38:
+            pakape = "I pakāpe"
+        elif 28 <= dalibnieka_vertejums["Punkti"] < 38:
+            pakape = "II pakāpe"
+        elif 18 <= dalibnieka_vertejums["Punkti"] < 28:
+            pakape = "III pakāpe"
+        else:
+            pakape = "Atzinība"
+        dalibnieka_vertejums["Pakāpe"] = pakape
+        rezultati.append(dalibnieka_vertejums)
     virsraksti = rezultati[0].keys()
+
 
     print(tabulate(rezultati, headers = 'keys', tablefmt="simple"))
     print()
@@ -96,6 +112,13 @@ def rezultati():
         print("Rezultāti saglabāti CSV datnē")
         print()
 
+    vai_novertets = input("Vai protokolā redzamos dalībniekus atzīmēt kā novērtētus?\nTie vairs nebūs pieejami žūrija vērtēšanai.\nj/n").lower().strip()
+    if vai_novertets == "j":
+        vaicajums = "UPDATE pieteikumi SET novertets = 1 WHERE novertets = 0"
+        conn.execute(vaicajums)
+        conn.commit()
+
+    db.slegt_savienojumu(conn)
 
 
 
